9ae1cbb06546520d5cb33fd6923ad4275eaae4da
mando-narin <y2pgftpe@keio.jp>
2021-03-21 13:37:58 +0900

    basicystems

---
+++ Assets/Scripts/RockController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RockController : MonoBehaviour
+{
+
+    float fallSpeed;
+	float rotSpeed;
+   // int tsuukacount =3;
+    GameObject rocket;
+    RockController rocketcontroller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.fallSpeed = 0.01f + 0.1f * Random.value;
+		this.rotSpeed = 5f + 3f * Random.value;
+        rocket= GameObject.Find("Rocket");
+      //  rocketcontroller = rocket.gameObject.GetComponent<RocketController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate( 0, -fallSpeed, 0, Space.World);
+		transform.Rotate(0, 0, rotSpeed );
+
+		if (transform.position.y < -5.5f) {
+			Destroy (gameObject);
+            // rocketcontroller = rocket.gameObject.GetComponent<RocketController>.Tsuu();
+            GameObject.Find ("rocket").GetComponent<RocketController> ().Tsuuka ();
+           // rocketcontroller.Tsuuka();
+		}
+       
+    }
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RocketController : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public int tsuukacount ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tsuukacount =3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+		}
+        if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+		}
+
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "sanso")
+		{
+            Kaihuku ();
+		}
+    }
+    public void Tsuuka(){
+        tsuukacount-=1;
+            Debug.Log(tsuukacount);
+		
+        if(tsuukacount==0){
+            SceneManager.LoadScene("Endscene");
+        }
+    }
+    public void Kaihuku(){
+        tsuukacount+=1;
+        Debug.Log(tsuukacount);
+    }
+}
+
